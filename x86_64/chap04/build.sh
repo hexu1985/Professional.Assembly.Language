49e0e452294a1573e6d8b93fce6e9a0c9d5c52a9
hexu <hexu@sensetime.com>
2020-07-21 11:04:42 +0800

    update README.md

--- x86_64/chap04/build.sh
+++ x86_64/chap04/build.sh
@@ -4,5 +4,5 @@ ld -o cpuid cpuid.o
 
 # build cpuid2.s
 as -g -o cpuid2.o cpuid2.s
-ld -e main -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o cpuid2 -lc cpuid2.o
+ld -o cpuid2 cpuid2.o -e main -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc 
 
